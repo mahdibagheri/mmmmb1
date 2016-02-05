cca4da46a539039944f027f2127f940888ff9f93
mahdibagheri <150mahdi@gmail.com>
2016-02-04 23:53:46 -0800

    Update creed.lua

--- bot/creed.lua
+++ bot/creed.lua
@@ -243,16 +243,8 @@ function create_config( )
     sudo_users = {77750573},--Sudo users
     disabled_channels = {},
     moderation = {data = 'data/moderation.json'},
-    about_text = [[Creed bot 2.3
-    
-     Hello my Good friends 
-     
-    ‼️ this bot is made by : @creed_is_dead
-   〰〰〰〰〰〰〰〰
-   ߔࠀ   our admins are : 
-   ߔࠀ   @sorblack_creed
-   ߔࠀ   @amircc_creed
-   ߔࠀ   @aria_creed
+    about_text = [[aqa mp3 v5.8
+@mahdimp3
    〰〰〰〰〰〰〰〰
   ♻️ You can send your Ideas and messages to Us By sending them into bots account by this command :
    تمامی درخواست ها و همه ی انتقادات و حرفاتونو با دستور زیر بفرستین به ما
